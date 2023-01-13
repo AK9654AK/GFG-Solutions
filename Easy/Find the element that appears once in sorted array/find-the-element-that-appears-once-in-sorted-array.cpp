200b703d7cc0069df3d20c1e3571adb32bb7fe67
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-01-13 23:38:56 +0530

    Update find-the-element-that-appears-once-in-sorted-array.cpp

--- Easy/Find the element that appears once in sorted array/find-the-element-that-appears-once-in-sorted-array.cpp
+++ Easy/Find the element that appears once in sorted array/find-the-element-that-appears-once-in-sorted-array.cpp
@@ -19,7 +19,35 @@ class Solution
         
         
         return ans;
+      
+      
+      
+      
+      
+      
+      
+      
+      
+//         // TC ---> O(logN)     SC ---> O(1)
+// 		int start=0;
+//         int end=n-2;
+//         while(start<=end){
+//             int mid=(start+end)>>1;
+//             if(arr[mid]==arr[mid^1])
+//                 start=mid+1;
+//             else
+//                 end=mid-1;
+//         }
+//         return arr[start];
         
+        
+        
+// // TC---> O(N)         SC ---> O(1)
+//          int ans=0;
+//          for(int i=0;i<n;i++){
+//              ans^=arr[i];
+//          }
+//          return ans;
         
         
         
