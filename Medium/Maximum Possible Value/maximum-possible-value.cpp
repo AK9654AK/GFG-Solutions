8b36779b8a9e39d6af27403cba6756a3590f7e3d
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-03-14 00:13:59 +0530

    Update maximum-possible-value.cpp

--- Medium/Maximum Possible Value/maximum-possible-value.cpp
+++ Medium/Maximum Possible Value/maximum-possible-value.cpp
@@ -8,16 +8,26 @@ using namespace std;
 class Solution {
   public:
     long long maxPossibleValue(int N,vector<int> A, vector<int> B) {
-     long long ans=0,t=0,z=0;
-        for(int i=0;i<N;i++)B[i]/=2;
-        int x=INT_MAX;
-        for(int i=0;i<N;i++){
-            if(B[i])x=min(A[i],x);
-            t+=B[i]*A[i];
-            z+=B[i];
-        }
-        if(z%2)t-=x;
-        return t*2;
+      long long ans=0;    ///Total length
+     int total=0;         //Total number of sticks;
+     
+     int mini=INT_MAX;  ////The stick with minimum size has to be removed at last hence mini;
+     
+     for(int i=0;i<N;i++){
+         int len=A[i];
+         int units=B[i];
+         
+         if(units%2!=0){   
+             units--;
+         }
+         if(units>=2)mini=min(mini,len);   ///If a stick is 1; then it become 0, due to which our answer will be unchanged;
+         ans+=(len*units);
+         total+=units;
+         
+     }
+     if(total%4!=0)ans-=(2*mini);     //if at last total length is not a multiple of 4 which means extra we need to remove;
+     
+     return ans;
     }
 };
 
