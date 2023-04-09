1f5a4316f7dbd6eb9c5657d35eb1ea29ac27ed13
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-04-09 14:44:57 +0530

    Updated Solution - GfG to GitHub

--- Medium/Next Greater Element/next-greater-element.cpp
+++ Medium/Next Greater Element/next-greater-element.cpp
@@ -6,99 +6,34 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
-    
-     int max(int a,int b){
-        if(a>b)return a;else return b;
-    }
-    
-    
-    
-    
-    
-    
     public:
     //Function to find the next greater element for each element of the array.
     vector<long long> nextLargerElement(vector<long long> arr, int n){
         
-        
-        
-        // int great=arr[n-1];
-        vector<long long> abhi;
-        // abhi.push_back(-1);
-        
-        // for(int i=n-2;i>=0;i--){
-            
-               
-               
-        //     great=max(arr[i+1],arr[i]);
-        //     abhi.push_back(great);
-            
-              /////////////////////////////////////////Wrong code;///////////////////
-            
-            
-        // }
-        
-        
-        // for(int i=0;i<n;i++){
-        //     arr[i];
-        // }
+        vector<long long>v;
+        stack<long long>s;
         
         
         
-        stack<long long> st;
-        abhi.push_back(-1);
-        st.push(arr[n-1]);
-        for(long long i=n-2;i>=0;i--){          /// USE LONG LONG
-            while(!st.empty() && (st.top()<=arr[i]))
-            st.pop();
-            
-            if(st.empty()){
-                abhi.push_back(-1);
-                st.push(arr[i]);
-            }
-            else{   //Striver SDE SHEET
-                abhi.push_back(st.top());
-                st.push(arr[i]);
-            }
+        for(long long i=n-1;i>=0;i--){
+           if(s.empty()==true)v.push_back(-1);
+           else if(s.top()>arr[i] and s.size()>0){
+               v.push_back(s.top());
+              
+           }
+           else if(s.top()<=arr[i] and s.size()>0){
+               while(s.size()>0 && s.top()<=arr[i])s.pop();
+               if(s.empty()==true)v.push_back(-1);
+               else v.push_back(s.top());
+           }
+           
+           s.push(arr[i]);
         }
         
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-        //   if(st.size()==0)abhi.push_back(-1);  // If its empty just put -1 to array/
-        //     else if(st.size()>0 && st.top()>arr[i]) 
-        //             abhi.push_back(st.top());  // If the condition is met then just put the top to array abhi
-        //     else if(st.size()>0 && st.top()<=arr[i]){
-        //           // if not greater then 
-        //         while(st.size()>0 && st.top()<=arr[i])
-        //               st.pop();
-        //         if(st.size()==0)abhi.push_back(-1);
-        //         else abhi.push_back(st.top());
-            
-        // }
         
-        // st.push(arr[i]);
-        // }
+        reverse(v.begin(),v.end());
         
-        
-        
-        reverse(abhi.begin(),abhi.end());
-        
-        return abhi;
+        return v;
         
         
         
@@ -106,23 +41,8 @@ class Solution
         
         
         
-//         stack<long long> st;
-// vector<long long> vec(n,-1);
-// for(int i=0;i<n;i++){
-//     while (st.size()!=0 && arr[i]>arr[st.top()]){
-//           vec[st.top()]=arr[i];
-//           st.pop();
-//     }
-//     st.push(i);
-// }
-// return vec;
         
-        
-        
-        
-        
-        
-        
+    
         
         
         
