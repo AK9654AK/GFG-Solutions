ab9fbe03b9c3b4c149879819720d5b1ba42b522b
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-07-03 22:16:23 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximum Index/maximum-index.cpp
+++ Medium/Maximum Index/maximum-index.cpp
@@ -7,28 +7,21 @@ using namespace std;
 class Solution{
 public:
     int maxIndexDiff(int arr[], int n) {
-    
-    int maximum=0;
-    
-//   int i=0;int j=n-1;
-   for(int i=0;i<n;i++){
-       
-   int j=n-1;
-   while(i<j){
-       
-       if(arr[i]<=arr[j]){
-           
-           maximum=max(maximum,j-i);
-           break;
-       }
-       
-       j--;
-   }
-   }
-    return maximum;
-   
+       stack<int> stk;
+        for(int i=n-1; i>=0; i--){
+            if(stk.empty()) stk.push(i);
+            else if(arr[i]>arr[stk.top()]) stk.push(i);
+        }
+        
+        int ans=0;
+        for(int i=0; i<n; i++){
+            while(!stk.empty() && arr[i] <= arr[stk.top()]){
+                ans = max(ans, stk.top()-i);
+                stk.pop();
+            }
+        }
+        return ans;
     }
-
 };
 
 
