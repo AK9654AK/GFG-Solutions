62a6cb2ab1440a4a372d971fedc74e2d66a3964a
AK9654AK <82321076+AK9654AK@users.noreply.github.com>
2023-08-02 23:34:22 +0530

    Updated Solution - GfG to GitHub

--- Medium/Reverse a Linked List in groups of given size/reverse-a-linked-list-in-groups-of-given-size.cpp
+++ Medium/Reverse a Linked List in groups of given size/reverse-a-linked-list-in-groups-of-given-size.cpp
@@ -46,6 +46,24 @@ void printList(struct node *node)
     }*head;
 */
 
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+*/
+
 class Solution
 {
     public:
