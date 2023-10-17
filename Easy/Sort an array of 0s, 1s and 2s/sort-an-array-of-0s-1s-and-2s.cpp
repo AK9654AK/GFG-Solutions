ab5cb2d5f1885c044a1f558c4a8f06c70ffa07ab
Abhishek K <82321076+AK9654AK@users.noreply.github.com>
2023-10-17 16:18:56 +0530

    Updated Solution - GfG to GitHub

--- Easy/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.cpp
+++ Easy/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.cpp
@@ -10,24 +10,37 @@ class Solution
     public:
     void sort012(int a[], int n)
     {
-         int low=0;
-       int mid=0;
-       int high=n-1;
-       
-       while(mid<=high)
-       {
-           if(a[mid]==0){
-               swap(a[low],a[mid]);
-               low++;
-               mid++;
-           }
-           else if(a[mid]==1)
-           mid++;
-           else if(a[mid]==2){
-               swap(a[mid],a[high]);
-               high--;
-           }
-       }
+        
+        int mid=0;int low=0;int high=n-1;
+        
+        
+        
+        while(mid<=high){
+            
+            
+            if(a[mid]==1)mid++;
+            else if(a[mid]==0){
+                swap(a[low],a[mid]);
+                mid++;
+                low++;
+                
+            }
+            else if(a[mid]==2){
+                swap(a[mid],a[high--]);
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+
+            
+
+        
     }
     
 };
