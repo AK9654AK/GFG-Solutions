b6929e7602ca3d3d2fd9d5ceb9e9d5ee6352c8ff
Abhishek K <82321076+AK9654AK@users.noreply.github.com>
2023-10-18 19:29:28 +0530

    Updated Solution - GfG to GitHub

--- Basic/Cyclically rotate an array by one/cyclically-rotate-an-array-by-one.cpp
+++ Basic/Cyclically rotate an array by one/cyclically-rotate-an-array-by-one.cpp
@@ -31,24 +31,14 @@ int main()
 
 void rotate(int arr[], int n)
 {
-    
-    
     int temp=arr[n-1];
     
-    for(int i=n-1;i>=0;i--){
-        
-        arr[i+1]=arr[i];
-        
+    
+    
+    
+    for(int i=n-2;i>=0;i--){
+        swap(arr[i+1],arr[i]);
     }
-    arr[0]=temp;
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    return;
 }
