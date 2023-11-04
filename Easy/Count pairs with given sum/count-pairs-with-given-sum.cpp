885d1ffc9179f10569c8e5fcd8a8145017210cdb
Abhishek K <82321076+AK9654AK@users.noreply.github.com>
2023-11-04 18:48:09 +0530

    Updated Solution - GfG to GitHub

--- Easy/Count pairs with given sum/count-pairs-with-given-sum.cpp
+++ Easy/Count pairs with given sum/count-pairs-with-given-sum.cpp
@@ -11,59 +11,20 @@ class Solution{
 public:
     int getPairsCount(int arr[], int n, int k) {
         
-        
-        // bool hash[100000000]={false};
-        
-        // for(int i=0;i<n;i++){
-            
-        //     hash[arr[i]]=true;
-            
-        // }
-        
-        
-        
-        // for(int i=0;i<n;i++){
-            
-        //     if(hash[k])
-            
-            
-        // }
-        
-        // int count=0;
-        
-        // int i=0;int j=n-1;
-        // sort(arr,arr+n);
-        
-        // while(i<j){
-        //     if(arr[i]+arr[j]==k)count++;
-        //     if(arr[i]+arr[j]>k)j--;
-        //     if(arr[i]+arr[j]<k)i++;
-            
-        // }
-        
-        // return 2*count;
-        
-        
-        int count=0;
-        
-       unordered_map<int,int> mp;
+         unordered_map<int,int> m;
        
+       int count=0;
        for(int i=0;i<n;i++){
-           mp[arr[i]]++;
-       }
-       
-       
-       for(int i=0;i<n;i++){
-           if(mp[k-arr[i]]>0){
-               
-               mp[arr[i]]--;
-               count+=mp[k-arr[i]];
+           int b=k-arr[i];
+           
+           if(m[b]){
+               count=count+m[b];
            }
+           m[arr[i]]++;
        }
        return count;
-        
-        
-    }
+   }
+    
 };
 
 //{ Driver Code Starts.
